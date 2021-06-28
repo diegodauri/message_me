1fe632e9fcda7ae2389e4e0cc7d210150895da00
Diego Dauri <dauridiego@gmail.com>
2021-06-28 11:36:42 +0200

    Add websocket config to production enviroment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.web_socket_server_url = "wss://message-me-diego.herokuapp.com/cable" 
+  config.action_cable.allowed_request_origins = ['https://message-me-diego.herokuapp.com', 'http://message-me-diego.herokuapp.com']
 end
